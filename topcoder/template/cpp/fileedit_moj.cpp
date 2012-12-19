b9c8e58e1265e318fc46765e185c01539923a26a
Aphrodite Spartacus <Aphrodite.Spartacus@gmail.com>
2012-12-19 20:44:41 +0800

    fighting

---
+++ topcoder/template/cpp/fileedit_moj.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class $CLASSNAME$
+{
+public:
+    $RC$ $METHODNAME$($METHODPARMS$);
+};
+$BEGINCUT$
+/////////////////////////////////////////////////////////////////////////////
+#ifdef DEBUG
+#include "/Users/Aphrodite/program/arena/libs/src/cpp/debug.cpp"
+#endif
+
+$TESTCODE$
+
+$DEFAULTMAIN$
+/////////////////////////////////////////////////////////////////////////////
+$ENDCUT$
+
+/****************************************************************************
+ *                                                                          *
+ *                To see the world in a grain of sand,                      *
+ *                And a heaven in a wild flower;                            *
+ *                Hold infinity in the palm of your hand,                   *
+ *                And eternity in an hour.                                  *
+ *                                                                          *
+ ***************************************************************************/
+
+
+$RC$ $CLASSNAME$ :: $METHODNAME$($METHODPARMS$)
+{
+
+}
