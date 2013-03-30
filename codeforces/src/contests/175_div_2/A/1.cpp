823fc5b3513266f6e3469c2a99594f7b4b65a227
Aphrodite Spartacus <Aphrodite.Spartacus@gmail.com>
2013-03-30 22:05:11 +0800

    fighting

--- codeforces/src/contests/175_div_2/A/1.cpp
+++ codeforces/src/contests/175_div_2/A/1.cpp
@@ -38,53 +38,22 @@ using namespace std;
  ***************************************************************************/
 
 
-pair<int, int> func(const vector<int> &v)
-{
-  pair<int, int> res;
-  for (size_t i = 1; i < (v).size(); ++i) {
-    if (v[i] == v[i - 1]) {
-      ++res.first;
-    } else {
-      ++res.second;
-    }
-  }
-  return res;
-}
-
 int main()
 {
-  int n, m;
-  cin >> n >> m;
-  map<int, int> mp;
-
-  vector< pair<int, int> > res;
-
-  for (int t = 0; t < 2; ++t) {
-    vector<int> v;
-    mp[0] = n;
-    mp[1] = m;
-
-    if (t == 0) {
-      v.push_back(0);
-      --mp[0];
-    } else {
-      v.push_back(1);
-      --mp[1];
+    int n, k;
+    cin >> n >> k;
+    vector<int> v1;
+    for (int i = 0; i < k; ++i) {
+        v1.push_back(n - i);
     }
-
-    for (int i = 1; i < n + m; ++i) {
-      int x = i % 2 == 0 ? v[i - 1] : 1 - v[i - 1];
-      if (mp[x] == 0) {
-        x = 1 - x;
-      }
-      v.push_back(x);
-      --mp[x];
+    for (int i = 0; i < n - k; ++i) {
+        v1.push_back(i + 1);
     }
-    #ifdef DEBUG
-    print(v);
-#endif
-    res.push_back(func(v));
-  }
-  sort((res).begin(), (res).end());
-  cout << res[1].first << " " << res[1].second << endl;
+    for (size_t i = 0; i < (v1).size(); ++i) {
+        if (i > 0) {
+            cout << " ";
+        }
+        cout << v1[i];
+    }
+    cout << endl;
 }
